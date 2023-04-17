7abf3b70472ccf3a535b9b4b9fa8ebfce558ce0c
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-04-17 18:54:40 -0300

    Update index.tsx

--- organo-typeScript/src/componentes/Colaborador/index.tsx
+++ organo-typeScript/src/componentes/Colaborador/index.tsx
@@ -13,6 +13,7 @@ const Colaborador = ({ nome, imagem, cargo, corDeFundo,data }:ColaboradorProps) 
         <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
             <img src={imagem} alt={nome}/>
         </div>
+        
         <div className='rodape'>
             <h4>{nome}</h4>
             <h5>{cargo}</h5>
