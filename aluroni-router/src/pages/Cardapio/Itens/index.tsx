e657676bb7d7130e8f45a987c4a6a21effec1fab
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-04-22 21:34:12 -0300

    routes cardapio/inicio

--- aluroni-router/src/pages/Cardapio/Itens/index.tsx
+++ aluroni-router/src/pages/Cardapio/Itens/index.tsx
@@ -1,4 +1,4 @@
-import cardapio from './itens.json'; 
+import cardapio from 'data/cadapio.json'; 
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useState, useEffect } from 'react';
---
+++ aluroni-router/src/pages/Inicio/index.tsx
@@ -0,0 +1,30 @@
+import cardapio from 'data/cadapio.json';
+import styles from './Inicio.module.scss';
+
+export default function Inicio()
+{
+  let pratosRecomendados = [ ...cardapio ];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).slice(0, 3);
+  return (
+    <section>
+      
+      <h3 className={styles.titulo}>
+        Recomendações da Cozinha
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div key={item.id} className={styles.recomendado}>
+
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+
+            <button className={styles.recomendado__botao}>
+              Ver Mais
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
