c7d80c972f113f5096429c3de10e6571b7df628a
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-05-09 18:39:15 -0300

    recoil

--- calendario-ToDo/src/components/Evento/EventoCheckbox/index.tsx
+++ calendario-ToDo/src/components/Evento/EventoCheckbox/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { IEvento } from '../../../interfaces/IEvento';
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
-  
+const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) =>
+{
+
   const estilos = [
     'far',
     'fa-2x',
     evento.completo ? 'fa-check-square' : 'fa-square'
+
   ]
 
   return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
