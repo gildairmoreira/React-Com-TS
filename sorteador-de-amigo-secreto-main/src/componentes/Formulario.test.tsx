e43e7809d99cda66a6524ed9b147f2651f804447
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-05-26 18:47:51 -0300

    teste do componente do input

--- sorteador-de-amigo-secreto-main/src/componentes/Formulario.test.tsx
+++ sorteador-de-amigo-secreto-main/src/componentes/Formulario.test.tsx
@@ -1,6 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
+import Formulario from './Formulario'
 
-test('', () =>
+test('quando o input está vazio, novos participantes não podem ser adicionados', () =>
 {
+    render(<Formulario />)
 
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('insira os nomes dos participante')
+
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+    // garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () =>
+{
+    render(<Formulario />)
+
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('insira os nomes dos participante')
+
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+
+    //inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Ana Catarina'
+        }
+    })
+
+    //clicar no botao de submeter
+    fireEvent.click(botao)
+
+    //garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+
+    //garantir que o input não tenha um valor
+    expect(input).toHaveValue("")
 })
